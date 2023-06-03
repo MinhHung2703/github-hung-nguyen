84c51017732912456886208b56eb478d28750d52
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2023-06-03 17:50:13 +0700

    make new updateCategory/GetAll/GetId/DeleteCategory

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -56,4 +56,60 @@ const updateCategoryController = async (req, res) => {
         });
     }
 };
-module.exports = { createCategoryController, updateCategoryController }
+
+// get All Category
+const categoryController = async (req, res) => {
+    try {
+        const category = await categoryModel.find({})
+        res.status(200).send({
+            success: true,
+            messsage: "All categories List",
+            category
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message: "Error while getting all categories",
+            error
+        })
+    }
+}
+
+const singleCategoryController = async (req, res) => {
+    try {
+        const category = await categoryModel.findOne({ slug: req.params.slug })
+        res.status(200).send({
+            success: true,
+            message: "Get Single Category Successfully",
+            category
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error while getting single Category",
+            error
+        });
+    }
+}
+
+const deleteCategoryController = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await categoryModel.findByIdAndDelete(id);
+        res.status(200).send({
+            success: true,
+            message: "Category Deleted Successfully",
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error while deleting category",
+            error
+        })
+    }
+}
+
+module.exports = { createCategoryController, updateCategoryController, categoryController, singleCategoryController, deleteCategoryController }
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,12 +1,21 @@
 const express = require("express")
 const { isAdmin, requireSignIn } = require("./../middlewares/authMiddleware.js");
-const { createCategoryController, updateCategoryController } = require("./../controllers/categoryController.js");
+const { createCategoryController, updateCategoryController, categoryController, singleCategoryController, deleteCategoryController } = require("./../controllers/categoryController.js");
 
 const router = express.Router()
 
-//create category
+// Create category
 router.post("/create-category", requireSignIn, isAdmin, createCategoryController);
 
-//Update Category
-router.put("/update-category/:id", requireSignIn, isAdmin, updateCategoryController)
+// Update Category
+router.put("/update-category/:id", requireSignIn, isAdmin, updateCategoryController);
+
+// Get All Category
+router.get('/get-category', categoryController)
+
+// Single Category
+router.get("/single-category/:slug", singleCategoryController)
+
+// Delect category
+router.delete("/delete-category/:id", requireSignIn, isAdmin, deleteCategoryController)
 module.exports = router
