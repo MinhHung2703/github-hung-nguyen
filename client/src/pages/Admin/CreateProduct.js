c4e403bfd361992a3cdfbea77681726db0f51be7
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2023-06-08 20:48:01 +0700

    have bugs in create-product page

--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -3,8 +3,8 @@ import Layout from "./../../components/layout/Layout";
 import AdminMenu from "./../../components/layout/AdminMenu";
 import toast from "react-hot-toast";
 import axios from "axios";
-import { Select } from "antd";
 import { useNavigate } from "react-router-dom";
+import { Select } from "antd";
 const { Option } = Select;
 
 const CreateProduct = () => {
@@ -18,49 +18,49 @@ const CreateProduct = () => {
     const [shipping, setShipping] = useState("");
     const [photo, setPhoto] = useState("");
 
-    // //get all category
-    // const getAllCategory = async () => {
-    //     try {
-    //         const { data } = await axios.get("/api/v1/category/get-category");
-    //         if (data?.success) {
-    //             setCategories(data?.category);
-    //         }
-    //     } catch (error) {
-    //         console.log(error);
-    //         toast.error("Something went wrong in getting catgeory");
-    //     }
-    // };
+    //get all category
+    const getAllCategory = async () => {
+        try {
+            const { data } = await axios.get("/api/v1/category/get-category");
+            if (data?.success) {
+                setCategories(data?.category);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("Something wwent wrong in getting catgeory");
+        }
+    };
 
-    // useEffect(() => {
-    //     getAllCategory();
-    // }, []);
+    useEffect(() => {
+        getAllCategory();
+    }, []);
 
-    // //create product function
-    // const handleCreate = async (e) => {
-    //     e.preventDefault();
-    //     try {
-    //         const productData = new FormData();
-    //         productData.append("name", name);
-    //         productData.append("description", description);
-    //         productData.append("price", price);
-    //         productData.append("quantity", quantity);
-    //         productData.append("photo", photo);
-    //         productData.append("category", category);
-    //         const { data } = axios.post(
-    //             "/api/v1/product/create-product",
-    //             productData
-    //         );
-    //         if (data?.success) {
-    //             toast.error(data?.message);
-    //         } else {
-    //             toast.success("Product Created Successfully");
-    //             navigate("/dashboard/admin/products");
-    //         }
-    //     } catch (error) {
-    //         console.log(error);
-    //         toast.error("something went wrong");
-    //     }
-    // };
+    //create product function
+    const handleCreate = async (e) => {
+        e.preventDefault();
+        try {
+            const productData = new FormData();
+            productData.append("name", name);
+            productData.append("description", description);
+            productData.append("price", price);
+            productData.append("quantity", quantity);
+            productData.append("photo", photo);
+            productData.append("category", category);
+            const { data } = axios.post(
+                "/api/v1/product/create-product",
+                productData
+            );
+            if (data?.success) {
+                toast.error(data?.message);
+            } else {
+                toast.success("Product Created Successfully");
+                navigate("/dashboard/admin/products");
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("something went wrong");
+        }
+    };
 
     return (
         <Layout title={"Dashboard - Create Product"}>
@@ -69,7 +69,7 @@ const CreateProduct = () => {
                     <div className="col-md-3">
                         <AdminMenu />
                     </div>
-                    <div className="col-md-9">
+                    <div className="col-md-9 m-5">
                         <h1>Create Product</h1>
                         <div className="m-1 w-75">
                             <Select
@@ -164,11 +164,11 @@ const CreateProduct = () => {
                                     <Option value="1">Yes</Option>
                                 </Select>
                             </div>
-                            {/* <div className="mb-3">
+                            <div className="mb-3">
                                 <button className="btn btn-primary" onClick={handleCreate}>
                                     CREATE PRODUCT
                                 </button>
-                            </div> */}
+                            </div>
                         </div>
                     </div>
                 </div>
