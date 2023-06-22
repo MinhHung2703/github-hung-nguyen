57ef1fd700f38364e91949a6f4deec8bf59810a5
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2023-06-22 16:14:37 +0700

    Doing Profile User Page

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -1,54 +1,54 @@
 import React, { useState, useEffect } from "react";
 import UserMenu from "../../components/Layout/UserMenu";
 import Layout from "../../components/Layout/Layout";
-// import { useAuth } from "../../context/auth";
-// import toast from "react-hot-toast";
-// import axios from "axios";
+import { useAuth } from "../../context/auth";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Profile = () => {
     //context
-    // const [auth, setAuth] = useAuth();
-    //state
-    // const [name, setName] = useState("");
-    // const [email, setEmail] = useState("");
-    // const [password, setPassword] = useState("");
-    // const [phone, setPhone] = useState("");
-    // const [address, setAddress] = useState("");
-    //get user data
-    // useEffect(() => {
-    //     const { email, name, phone, address } = auth?.user;
-    //     setName(name);
-    //     setPhone(phone);
-    //     setEmail(email);
-    //     setAddress(address);
-    // }, [auth?.user]);
+    const [auth, setAuth] = useAuth();
+    // state
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+    const [address, setAddress] = useState("");
+    // get user data
+    useEffect(() => {
+        const { email, name, phone, address } = auth?.user;
+        setName(name);
+        setPhone(phone);
+        setEmail(email);
+        setAddress(address);
+    }, [auth?.user]);
 
     // form function
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     try {
-    //         const { data } = await axios.put("/api/v1/auth/profile", {
-    //             name,
-    //             email,
-    //             password,
-    //             phone,
-    //             address,
-    //         });
-    //         if (data?.errro) {
-    //             toast.error(data?.error);
-    //         } else {
-    //             setAuth({ ...auth, user: data?.updatedUser });
-    //             let ls = localStorage.getItem("auth");
-    //             ls = JSON.parse(ls);
-    //             ls.user = data.updatedUser;
-    //             localStorage.setItem("auth", JSON.stringify(ls));
-    //             toast.success("Profile Updated Successfully");
-    //         }
-    //     } catch (error) {
-    //         console.log(error);
-    //         toast.error("Something went wrong");
-    //     }
-    // };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await axios.put("/api/v1/auth/profile", {
+                name,
+                email,
+                password,
+                phone,
+                address,
+            });
+            if (data?.error) {
+                toast.error(data?.error);
+            } else {
+                setAuth({ ...auth, user: data?.updatedUser });
+                let ls = localStorage.getItem("auth");
+                ls = JSON.parse(ls);
+                ls.user = data.updatedUser;
+                localStorage.setItem("auth", JSON.stringify(ls));
+                toast.success("Profile Updated Successfully");
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("Something went wrong");
+        }
+    };
     return (
         <Layout title={"Your Profile"}>
             <div className="container-fluid m-3 p-3 dashboard">
@@ -58,7 +58,7 @@ const Profile = () => {
                     </div>
                     <div className="col-md-8">
                         <div className="form-container" style={{ marginTop: "-40px" }}>
-                            {/* <form onSubmit={handleSubmit}>
+                            <form onSubmit={handleSubmit}>
                                 <h4 className="title">USER PROFILE</h4>
                                 <div className="mb-3">
                                     <input
@@ -116,7 +116,7 @@ const Profile = () => {
                                 <button type="submit" className="btn btn-primary">
                                     UPDATE
                                 </button>
-                            </form> */}
+                            </form>
                         </div>
                     </div>
                 </div>
